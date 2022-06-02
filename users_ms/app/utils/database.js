cad326d93fbeef9a80147d0e8723199532f3ede2
Aleksandar Lekov <56325705+shonie2xx@users.noreply.github.com>
2022-06-02 21:36:34 +0200

    empty

--- users_ms/app/utils/database.js
+++ users_ms/app/utils/database.js
@@ -2,7 +2,5 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(`postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOSTNAME}:${process.env.PG_PORT}/${process.env.PG_DB_NAME}`)
-//const sequelize = new Sequelize(`postgres://alex:12345@34.90.139.74:5432/db_users`)
-
 
 module.exports = sequelize;
